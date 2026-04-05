b353f8d52c447f7df3b0addf30375cd5574f3f1f
vijaysarma1007 <svmsarma53@gmail.com>
2026-04-05 17:31:00 +0530

    added generics to traits , super traits and sub traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
+trait Investment<T> {
+    fn amount(&self) -> T;
+
+    fn double_amount(&mut self);
+}
+
+trait Taxable: Investment<f64> {
+    const TAX_RATE: f64 = 0.25;
+
+    fn tax_bill(&self) -> f64 {
+        self.amount() * Self::TAX_RATE
+    }
+}
+
+#[derive(Debug)]
+struct Income {
+    amount: f64,
+}
+
+impl Investment<f64> for Income {
+    fn amount(&self) -> f64 {
+        self.amount
+    }
+
+    fn double_amount(&mut self) {
+        self.amount *= 2.0;
+    }
+}
+
+impl Taxable for Income {}
+
+#[derive(Debug)]
+struct Bonus {
+    amount: f64,
+}
+
+impl Investment<f64> for Bonus {
+    fn amount(&self) -> f64 {
+        self.amount
+    }
+
+    fn double_amount(&mut self) {
+        self.amount *= 2.0;
+    }
+}
+
+impl Taxable for Bonus {
+    const TAX_RATE: f64 = 0.50;
+}
+
+#[derive(Debug)]
+struct QualityTime {
+    minutes: u32,
+}
+
+impl Investment<u32> for QualityTime {
+    fn amount(&self) -> u32 {
+        self.minutes
+    }
+
+    fn double_amount(&mut self) {
+        self.minutes *= 2;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut income = Income { amount: 50000.50 };
+    println!("Total tax owed: ${:.2}", income.tax_bill());
+    income.double_amount();
+    println!("Total tax owed: ${:.2}", income.tax_bill());
+
+    let bonus = Bonus { amount: 10000.23 };
+    println!("Total tax owed: ${:.2}", bonus.tax_bill());
+
+    let weekend = QualityTime { minutes: 120 };
+    println!("Relaxation time: {:.2}", weekend.amount());
 }
